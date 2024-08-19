ac916c1333ce83da845d1c4ab58b8caca0d11282
Brainergybyleo <leonard6oba@gmail.com>
2024-08-19 09:43:02 +0100

    added new components dropdown, Avatar, Button.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,19 +2,41 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "../img/dark.png";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 
 const Navbar = () => {
-  return (
-    <div className="bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between ">
-      <Link href="/">
-        <Image src={logo} alt="Brainergybyleo" width={40} />
+    return (
+        <div className="bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between ">
+            <Link href="/">
+                <Image src={logo} alt="Brainergybyleo" width={40} />
+            </Link>
 
-        <Avatar>
-          <AvatarImage src="https:github.com/shadcn.png" />
-        </Avatar>
-      </Link>
-    </div>
-  );
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                        <AvatarFallback className="text-black">BT</AvatarFallback>
+                    </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                    <DropdownMenuItem>Billing</DropdownMenuItem>
+                    <DropdownMenuItem>Team</DropdownMenuItem>
+                    <DropdownMenuItem>Subscription</DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
+    );
 };
 
 export default Navbar;
