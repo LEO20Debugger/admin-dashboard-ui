8ef8c15632a4c6c87d10e523ef06f8f11f8587ad
Brainergybyleo <leonard6oba@gmail.com>
2024-08-21 19:24:37 +0100

    modified components in UI folder

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import DashboardCard from "@/components/dashboard/DashboardCard";
 
 export default function Home() {
   return (
-   <>
-   <div className="flex flex-col justify-between"></div>
-   </>
+    <>
+      <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
+        <DashboardCard />
+      </div>
+    </>
   );
 }
--- components/dashboard/DashboardCard.tsx
+++ components/dashboard/DashboardCard.tsx
@@ -1,5 +1,22 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Newspaper } from "lucide-react";
+
 const DashboardCard = () => {
-    return (<div>Card</div>);
+    return (
+        <Card className="bg-slate-100 dark:bg-slate-100 p-4 pb-0">
+            <CardContent>
+                <h3 className="text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200">
+                    Posts
+                </h3>
+                <div className="flex gap-5 justify-center items-center">
+                    <Newspaper className="text-slate-500" size={72} />
+                    <h3 className="text-5xl font-semibold text-slate-500 dark:text-slate-200">
+                        100
+                    </h3>
+                </div>
+            </CardContent>
+        </Card>
+    );
 };
 
 export default DashboardCard;
